115eb842a08029721a5b3ac377cbbad813e6a8d5
wangdongkai <973153384@qq.com>
2017-03-17 10:56:25 +0800

    1.json动态加载控制器

--- WDKWeiBo/WDKWeiBo/Classes/Main/MainTabBarController.swift
+++ WDKWeiBo/WDKWeiBo/Classes/Main/MainTabBarController.swift
@@ -18,11 +18,42 @@ class MainTabBarController: UITabBarController {
 //        addChildViewController(childVC: MessageViewController(), imageName: "tabbar_message_center", title: "消息")
 //        addChildViewController(childVC: ProfileViewController(), imageName: "tabbar_profile", title: "我")
 
-        addChildViewController(childControllerName: "HomeViewController", imageName: "tabbar_home", title: "首页")
-        addChildViewController(childControllerName: "DiscoverViewController", imageName: "tabbar_discover", title: "发现")
-        addChildViewController(childControllerName: "MessageViewController", imageName: "tabbar_message_center", title: "消息")
-        addChildViewController(childControllerName: "ProfileViewController", imageName: "tabbar_profile", title: "我")
+//        addChildViewController(childControllerName: "HomeViewController", imageName: "tabbar_home", title: "首页")
+//        addChildViewController(childControllerName: "DiscoverViewController", imageName: "tabbar_discover", title: "发现")
+//        addChildViewController(childControllerName: "MessageViewController", imageName: "tabbar_message_center", title: "消息")
+//        addChildViewController(childControllerName: "ProfileViewController", imageName: "tabbar_profile", title: "我")
 
+        guard let jsonPath = Bundle.main.path(forResource: "MainVCSettings.json", ofType: nil) else {
+            
+            return
+        }
+        
+        guard let data = try? NSData(contentsOfFile: jsonPath, options: NSData.ReadingOptions.alwaysMapped) else {
+            
+            return
+        }
+        
+        guard let jsonArray = try? JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.mutableLeaves) as? [[String: Any]] else {
+            
+            return
+        }
+        
+        for dict in jsonArray! {
+            
+            guard let vcName = dict["vcName"] as? String else {
+                continue
+            }
+            
+            guard let imageName = dict["imageName"] as? String else {
+                continue
+            }
+
+            guard let titleName = dict["title"] as? String else  {
+                continue
+            }
+
+            addChildViewController(childControllerName: vcName, imageName: imageName, title: titleName)
+        }
     }
 
     // 方法重载->1.参数名相同但是类型不同；2.参数相同但是数量不同
@@ -60,6 +91,7 @@ class MainTabBarController: UITabBarController {
         let childType = UIViewController.self
         
         let childVC = childType.init()
+        childVC.view.backgroundColor = UIColor.white
         
         childVC.tabBarItem.image = UIImage(named: imageName)
         childVC.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
