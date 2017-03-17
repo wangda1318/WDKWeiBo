1d57cccdb27d34d4d882789c259474b9c55ad5b0
wangdongkai <973153384@qq.com>
2017-03-17 12:32:14 +0800

    1.使用storyboard refrence创建控制器

--- WDKWeiBo/WDKWeiBo/AppDelegate.swift
+++ WDKWeiBo/WDKWeiBo/AppDelegate.swift
@@ -15,16 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainTabBarController()
+        // Override point for customization after application launch
         
         // 全局设置tabBar颜色
         UITabBar.appearance().tintColor = UIColor.orange
-        
-        window?.makeKeyAndVisible()
-        
+                
         return true
     }
 
