92a530155c1cb66088afc5f507dec11090e5e7c7
武汉云升科技 <yskj0821@163.com>
2017-04-22 10:22:40 +0800

    1.添加底部工具栏

--- WDKWeiBo/WDKWeiBo/Classes/Main/ComposeViewController.swift
+++ WDKWeiBo/WDKWeiBo/Classes/Main/ComposeViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class ComposeViewController: UIViewController {
 
     @IBOutlet weak var composeTextView: CustomTextView!
+    
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    @IBOutlet weak var toolBarBCon: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigation()
+        
+        // MARK: - 键盘
+
+        NotificationCenter.default.addObserver(self, selector: #selector(ComposeViewController.keyboardWillShow(noticifation:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ComposeViewController.keyboardDidHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
         // Do any additional setup after loading the view.
     }
@@ -25,6 +35,11 @@ class ComposeViewController: UIViewController {
         
         composeTextView.becomeFirstResponder()
         
+    }
+    
+    deinit {
+        
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
@@ -38,6 +53,37 @@ private extension ComposeViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "发布", style: .plain, target: self, action: #selector(ComposeViewController.send))
         navigationItem.rightBarButtonItem?.isEnabled = false
         
+        
+        
+    }
+    
+    
+}
+
+extension ComposeViewController {
+    
+    @objc func keyboardWillShow(noticifation: NSNotification) {
+        
+        
+        print(noticifation.userInfo!)
+
+        let frame = (noticifation.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        toolBarBCon.constant = frame.height
+        
+        UIView.animate(withDuration: 0.25) { 
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardDidHide() {
+        
+        toolBarBCon.constant = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+
     }
 }
 
